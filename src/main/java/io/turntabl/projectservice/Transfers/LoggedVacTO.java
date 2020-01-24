b541991110478386f4edb1bff6595d951b3fa6e2
FrancisBilla <francis.billa.fb@gmail.com>
2020-01-24 11:58:55 +0000

    new re

--- src/main/java/io/turntabl/projectservice/Transfers/LoggedVacTO.java
+++ src/main/java/io/turntabl/projectservice/Transfers/LoggedVacTO.java
@@ -6,6 +6,9 @@ public class LoggedVacTO {
 
     private Integer employee_id;
     private java.sql.Date vacation_date;
+    private String employee_firstname;
+    private String employee_lastname;
+    private String employee_email;
 
 
     public LoggedVacTO() {
@@ -26,6 +29,31 @@ public class LoggedVacTO {
 
     public void setVacation_date(Date vacation_date) {
         this.vacation_date = vacation_date;
+    }
+
+
+    public String getEmployee_firstname() {
+        return employee_firstname;
+    }
+
+    public void setEmployee_firstname(String employee_firstname) {
+        this.employee_firstname = employee_firstname;
+    }
+
+    public String getEmployee_lastname() {
+        return employee_lastname;
+    }
+
+    public void setEmployee_lastname(String employee_lastname) {
+        this.employee_lastname = employee_lastname;
+    }
+
+    public String getEmployee_email() {
+        return employee_email;
+    }
+
+    public void setEmployee_email(String employee_email) {
+        this.employee_email = employee_email;
     }
 }
 
