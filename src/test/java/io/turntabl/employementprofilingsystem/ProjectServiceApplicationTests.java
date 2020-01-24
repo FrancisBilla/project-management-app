836a66a8f2c7f36f63d80d237dcd2307660cf015
Samuel Osei Kwakye <kwakyesamuelosei@gmail.com>
2020-01-24 01:46:30 +0000

    Deploy to AWS

---
+++ src/test/java/io/turntabl/employementprofilingsystem/ProjectServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package io.turntabl.employementprofilingsystem;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectServiceApplicationTests {
+
+	@Test
+	void addEmployee() {
+	}
+
+	@Test
+	void getAllEmployeeProfile() {
+	}
+
+}
